9e77220ca0416c7cb91a6517ff35e626269a47e3
Andrew-Nekowitsch <anekowitsch@gmail.com>
2022-03-17 15:22:13 -0500

    scaled party to browser size

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Advertisements } from './components/overwatch/main/advertisements/Advertisements'
-import { Logo } from './components/overwatch/main/logo/Logo'
-import { Party } from './components/overwatch/main/party/Party'
-import { MainNav } from './components/overwatch/main/nav/MainNav'
-import { MainChat } from './components/overwatch/main/chat/MainChat'
+import { Advertisements } from './components/overwatch/main/advertisements/Advertisements';
+import { Logo } from './components/overwatch/main/logo/Logo';
+import { Party } from './components/overwatch/main/party/Party';
+import { MainNav } from './components/overwatch/main/nav/MainNav';
+import { MainChat } from './components/overwatch/main/chat/MainChat';
 
-import './styles/index.css'
+import MenuImage from './assets/ow/images/main-menu.jpg';
+
+import './styles/index.css';
 
 export const App = () => {
-  return (
-    <>
-      <div className="bg-image"></div>
-
-      <div className="overwatch">
-        <Logo />
-        <Party />
-        <MainNav />
-        <Advertisements />
-        <MainChat />
-      </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div className='overwatch v-h-center'>
+				<img src={MenuImage} alt='overwatch ui' className='bg-image'></img>
+				<Logo />
+				<Party />
+				<MainNav />
+				<Advertisements />
+				<MainChat />
+			</div>
+		</>
+	);
+};
